aa5bad9c2632f0c7f38d11e74d9a89df120a81d1
Oskar <b00792974@essec.edu>
2023-03-08 13:50:36 +0100

    added folders src and use_cases

--- src/privacy_check/privacy_check.py
+++ src/privacy_check/privacy_check.py
@@ -1,6 +1,30 @@
 # Import libraries
+from sdmetrics.reports.single_table import DiagnosticReport
 
 
-class PrivacyCheck:
+class PrivacyCheck(DiagnosticReport):
+    """
+    Class to generate a report on the synthetic data, that checks 
+    how similar it is to the original one, with respect to privacy
+    concerns.
+
+    For the moment the class only inherits of DiagnosticReport.
+    
+    Attributes:
+        - Synthesis, Coverage, Boundaries scores
+        - Real data
+        - Sythetic Data
+        - Datatypes of columns
+
+    Methods:
+        - Create Report (-> compute Synthesis, Coverage, Boundaries scores)
+        - Get details on report (return summary of report)
+        - Get individual scores
+        - Visualizations
+        - Save report as a file
+ 
+    """
     def __init__(self):
-        
+        super().__init__()
+    
+
