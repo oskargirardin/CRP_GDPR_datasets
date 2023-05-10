1f4ca5864f705f6a9d4f73709ba57220726bb7cb
lucreziacerto <124700984+lucreziacerto@users.noreply.github.com>
2023-05-10 18:06:34 +0800

    Add files via upload

--- src/dataframe_summary/df_summary.py
+++ src/dataframe_summary/df_summary.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Author: Lucrecerto
-Date: 10/5/2023
+Created on Wed May 10 11:39:05 2023
 
-Description:
-This module contains a function for generating a summary of a pandas dataframe. The function takes in a dataframe and a file name, and generates a summary dataframe with the following information:
-- names of the columns
-- data types of the columns
-- randomly selected example to show the column's formatting
-
-Dependencies:
-- pandas
-- random
-
-Usage:
-Call the dataframe_summary() function with the dataframe to summarize and the desired file name for the summary output. The function returns the summary dataframe and also saves it to a CSV file.
-
-Warning: HERE LUCRE ADDS A COMMENT TO EXPLAIN WHY WE PRINT THE VARIABLES THAT CONTAIN MULTIPLE DATATYPES
+@author: lucreziacerto
 """
 
 import pandas as pd
@@ -30,11 +16,13 @@ def dataframe_summary(df, file_name):
     df: dataframe to summarize
     file_name: name of the file the summary should be saved in. Of the format 'file_name.csv'
     
-    Returns: summary dataframe with the following information:
-        - names of the columns 
-        - data types fo the columns
-        - randomly selected example to show the column's formatting
-    
+    Prints:
+        Number of rows of the dataset
+        Dataset summary (column names, columns data type, example)
+        Columns that have multiple datatypes
+        
+    Saves:
+        Dataset summary as a csv file
     '''
     summary = pd.DataFrame()
 
@@ -47,6 +35,18 @@ def dataframe_summary(df, file_name):
       #example is randomly selected among all the occurrences in the column
       x = random.randint(0, len(summary)-1)
       summary.at[i, 'example'] = str(df.loc[x,col_name])
+      
+    
   
     summary.to_csv(file_name)
-    return summary
+    print('Number of rows of the dataset:', len(df),'\n')
+    print('---- DATAFRAME SUMMARY -----')
+    print(summary)
+    print('---------')
+    
+    #Checking if there are columns with multiples data types - it becomes an issue in data generation
+    print('\nColumns with different datatypes')
+    for col in df.columns:
+        unique_types = df[col].apply(type).unique()
+        if len(unique_types) > 1:
+            print(col, unique_types)
