75f40dcdc16c8509bad8e13177fbfe9603a48b8f
Oskar <b00792974@essec.edu>
2023-05-23 12:01:16 +0200

    added documentation and incorporated sensitive column feature

--- src/privacy_check/privacy_check.py
+++ src/privacy_check/privacy_check.py
@@ -61,79 +61,99 @@ class PrivacyCheck(DiagnosticReport):
         return super().get_properties()
     
 
-    def find_nearest_neighbours(self, verbose = True):
+    def _get_column_types(self):
         """
-        Function to generate privacy score based on nearest neighbours
+        Function that gets the column types of the data
+
+        return: numerical columns (list), categorical columns (list)
         """
-        pairs = self._find_nn(verbose=verbose)
-        ##self.score = score
-        self.pairs = pairs
+        dtypes = {col: col_type["type"] for col, col_type in self.metadata["fields"].items()}
+        numeric_cols = [col for col, type in dtypes.items() if type == "numerical"]
+        cat_cols = [col for col, type in dtypes.items() if type != "numerical"]
+        return numeric_cols, cat_cols
 
 
-
-
-    def _find_nn(self, verbose = True):
+    def _filter_columns(self, columns, *dfs):
         """
-        Function that computes the privacy score of the synthetic data
+        Function that subsets dataframes to only include given columns
 
-        :param only_cat: boolean that indicates if we should only consider categorical columns to compute the distance
+        :param columns: columns that should be included
+        :param *dfs: dataframes that should be subsetted
+        """
+        for df in dfs:
+            for col in df.columns:
+                if col not in columns:
+                    df.drop(col, axis = 1, inplace = True)
+
+
+    def find_nearest_neighbours(self, sensitve_columns = None, verbose = True):
+        """
+        Function that finds the nearest neighbours of every synthetic row. Rows with NA values have a distance of NA as well.
+
+        :param verbose (optional): boolean that controls the verbosity of the output
+        :param sensitve_columns (optional): list to specify columns that should play a role for distance computation
 
         return: list of nearest neighbours of all sythentic rows
         """
         # Initialization
-        nneighbour_idx = []
+        neighbour_pairs = []
         n_samples = len(self.synthetic_data)
         df_real, df_synth = self.original_data.copy(), self.synthetic_data.copy()
 
         # Separate dataframes into numerical and categorical
-        dtypes = {col: col_type["type"] for col, col_type in self.metadata["fields"].items()}
-        numeric_cols = [col for col, type in dtypes.items() if type == "numerical"]
-        cat_cols = [col for col, type in dtypes.items() if type != "numerical"]
-        df_real_num = df_real[numeric_cols]
-        df_synth_num = df_synth[numeric_cols]
-        df_real_cat = df_real[cat_cols]
-        df_synth_cat = df_synth[cat_cols]
+        numeric_cols, cat_cols = self._get_column_types()
+        df_real_num, df_real_cat = df_real[numeric_cols], df_real[cat_cols]
+        df_synth_num, df_synth_cat = df_synth[numeric_cols], df_synth[cat_cols]
         
-        # Normalize numeric columns -> equal weights to each col
+        # Normalize numeric columns to give equal weights to each column
         scaler = StandardScaler()
-        df_real_num = pd.DataFrame(scaler.fit_transform(df_real_num))
-        df_synth_num = pd.DataFrame(scaler.transform(df_synth_num))
+        df_real_num = pd.DataFrame(scaler.fit_transform(df_real_num), columns=numeric_cols)
+        df_synth_num = pd.DataFrame(scaler.transform(df_synth_num), columns=numeric_cols)
         
-        # Loop over rows in synthetic dataset
-        for idx_synth in tqdm.tqdm(range(n_samples), desc='Computing privacy score', disable=(not verbose)):
+        if not sensitve_columns is None:
+            self._filter_sensitive_columns(sensitve_columns, df_real_num, df_real_cat, df_synth_num, df_synth_cat)
+
+        #df_real_num, df_real_cat, df_synth_num, df_synth_cat = df_real_num, df_real_cat, df_synth_num, df_synth_cat
+
+        # For every synthetic row, find its nearest neighbour
+        for idx_synth in tqdm.tqdm(range(n_samples), desc='Finding nearest neighbours', disable=(not verbose)):
             # Get numerical elements and categorical elements
-            row_num = df_synth_num.iloc[idx_synth]
-            row_cat = df_synth_cat.iloc[idx_synth]
+            row_num = df_synth_num.iloc[idx_synth].to_numpy()
+            row_cat = df_synth_cat.iloc[idx_synth].to_numpy()
             # Compute distance to every real row 
             # TODO: Give more importance to dists_cat, because dists_num > dists_cat in general
-            dists_num = np.sum(np.abs(df_real_num - row_num), axis = 1)
-            dists_cat = np.sum(df_real_cat != row_cat, axis = 1)
+            dists_num = np.sum(np.abs(df_real_num.to_numpy() - row_num), axis = 1)
+            dists_cat = np.sum(df_real_cat.to_numpy() != row_cat, axis = 1)
             dists = (dists_num + dists_cat) / (len(numeric_cols) + len(cat_cols))
             # Find minimal distance and append neighbour index
-            min_dist = dists.min()
-            idx_neighbour = dists.argmin()
-            nneighbour_idx += [(idx_synth, idx_neighbour, min_dist)]
+            min_dist = np.min(dists)
+            idx_neighbour = np.argmin(dists)
+            neighbour_pairs += [(idx_synth, idx_neighbour, min_dist)]
 
-        # OUTDATED: 
-        # Score is determined only by the closest distance among all nearest neighbours
-        # For privacy, we need every synthetic row to be sufficiently different from every real row.
-        #score = 1 - min(dists_nn)
+        self.pairs = neighbour_pairs
 
+        return neighbour_pairs
 
-        return nneighbour_idx
-
-    def display_k_closest_pairs(self, k):
+    def display_closest_pairs(self, k):
         """
         Displays the k closest pairs
 
         :param k (int): number of closest pairs to display 
         """
-        pairs_sorted = sorted(self.pairs, key = lambda x: x[2])
+        assert k > 0, "k must be larger than 0"
+        try:
+            pairs_sorted = sorted(self.pairs, key = lambda x: x[2])
+        except AttributeError:
+            raise Exception("self.pairs is not defined, please run function find_nearest_neighbours first.")
         k_closest_pairs = pairs_sorted[:k]
+        print(f"############ TOP {k} CLOSEST PAIRS ############")
         for i in range(k):
-            print(f"############ TOP {k} CLOSEST PAIRS ############")
             idx_synth, idx_neighbour, dist = k_closest_pairs[i]
             df = pd.concat([self.synthetic_data.iloc[idx_synth], self.original_data.iloc[idx_neighbour], ], axis = 1)
             df.columns = [f"Synthetic obs. (idx: {idx_synth})", f"Closest real obs. (idx: {idx_neighbour})"]
             print(f"{i+1}. Closest pair with distance: {dist: .4f}")
             print(df)
+
+    
+    def delete_closest_synthetic_columns(self, k):
+        pass
